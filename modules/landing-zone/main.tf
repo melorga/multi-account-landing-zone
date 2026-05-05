c1564f42b6c837e520b6c3d0397b6a8feed61865
Mariano <13036175+melorga@users.noreply.github.com>
2026-05-05 07:22:09 +0200

    refactor(modules): extract terraform block into versions.tf
    Move the terraform { required_version / required_providers } block out
    of main.tf into a dedicated versions.tf, the conventional location for
    Terraform module version constraints. Also declare hashicorp/random
    explicitly (used by random_string.suffix).

--- modules/landing-zone/main.tf
+++ modules/landing-zone/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.9, < 2.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 # Data source for current AWS account
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
